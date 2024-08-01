4f3ce012c6c798273a00eb92daefd519119bd724
Pawel Kierzek <pkierzek8@gmail.com>
2024-08-01 17:53:02 +0200

    test: example test

---
+++ test/second_test.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+int main(void)
+{
+    int x = 1 + 1;
+
+    return (x == 2) ? 0 : 1;
+}
---
+++ test/test_main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+
+
+int main(void)
+{
+    int x = 1 + 1;
+
+    return (x == 2) ? 0 : 1;
+}
