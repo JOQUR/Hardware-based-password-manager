d35eb15f25515cf47e6852c69108684ca4eaa82c
Pawel Kierzek <pkierzek8@gmail.com>
2024-08-13 16:51:04 +0200

    fix: add missing files

--- generate_messaging.sh
+++ generate_messaging.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cd ./messaging_bp
-mkdir inc
-mkdir src
-bitproto c messaging.bitproto
-mv messaging_bp.c   ./src/messaging_bp.c
-mv messaging_bp.h   ./inc/messaging_bp.h
-cd ..
+mkdir ./messaging_bp/inc
+mkdir ./messaging_bp/src
+bitproto c ./messaging_bp/messaging.bitproto
+mv ./messaging_bp/messaging_bp.c   ./messaging_bp/src/messaging_bp.c
+mv ./messaging_bp/messaging_bp.h   ./messaging_bp/inc/messaging_bp.h
